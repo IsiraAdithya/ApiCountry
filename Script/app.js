06e21099de6e1dd6a7e6bd01423745d55a82831e
IsiraAdithya <isiradabare1230@gmail.com>
2024-07-03 14:28:32 +0530

    Aded Cards

--- Script/app.js
+++ Script/app.js
@@ -20,10 +20,10 @@ fetch("https://restcountries.com/v3.1/all")
 
 
 
-// https://restcountries.com/v3.1/name/{name}
 
 
-function serchCuntrie() {
+
+function serchcountries() {
     let searchValue = document.getElementById("txtSearchValue").value;
 
     let officialName = document.getElementById("officialName");
@@ -48,4 +48,31 @@ function serchCuntrie() {
 
 
         })
+}
+
+function serchcountriestocard(){
+    let searchValue = document.getElementById("txtSearchValue").value;
+    let countryName = document.getElementById("countryName");
+    let officialName = document.getElementById("officialName");
+    let name = document.getElementById("name");
+    let img = document.getElementById("img");
+
+    fetch(`https://restcountries.com/v3.1/name/${searchValue}`)
+    .then(res => res.json())
+    .then(data => {
+
+        data.forEach(obj => {
+            document.getElementById("countryName").value = obj.name.common;
+            name.innerText = obj.name.common;
+
+            doucumet.getElementById("countryFlag").value = `<img src="${obj.flags.png}" alt="">`
+        })
+        console.log(data);
+
+
+
+
+    })
+
+    
 }
